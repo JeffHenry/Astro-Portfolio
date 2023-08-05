34a806d81b03763dee5e4de3e8b262eddf9d69a4
JeffHenry <jcurtis.henry@gmail.com>
2023-08-04 20:53:57 -0700

    Initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,111 @@
+export const info = {
+  baseUrl: 'https://portfolio.jeffreyhenry.me',
+  name: 'Jeffrey Henry',
+  jobDescription: 'Full Stack Claris Developer',
+  about: `Certified and experienced full stack developer with 7+ years architecting software solutions to enhance business productivity in various industries with the Claris platform and web languages. Well versed with all aspects of the software development lifecycle including requirement gathering, prototyping, scope estimation, development, quality assurance, and support. Recognized as a competent leader who works well within team environments and remote.`,
+
+  experience: [
+    {
+      name: 'Pacific Solutions',
+      title: 'Software Engineer',
+      location: 'Franklin, TN (Remote)',
+      startDate: 'Mar 2020',
+      endDate: 'Present',
+      description: [
+        '- Developed new feature and versions of Jobrunner and Floormanager solution',
+        '- Fixed reported bugs and refactored outdated integrations and workflows for improved performance',
+        '- Wrote custom estimates for client requests and new features',
+        '- Integrated rest APIs (DocuSign, Tableau, Google Charts, JotForms, B2B)',
+        '- Integrated third-party plug-ins (FTP, Email, File Manipulation, URL Query)',
+      ],
+    },
+
+    {
+      name: 'Productive Computing, Inc.',
+      title: 'Software Engineer, L3',
+      location: 'San Marcos, CA',
+      startDate: 'Jul 2018',
+      endDate: 'Mar 2020',
+      description: [
+        '- Provided client consultations and estimates for custom solution projects.',
+        '- Developed custom legacy and vertical solutions based on client needs.',
+        '- Integrated third-party APIs and plug-ins with client solutions.',
+        '- Sprint planned projects and managed L1 and L2 developers.',
+      ],
+    },
+
+    {
+      name: 'Productive Computing, Inc.',
+      title: 'Product and Hosting Engineer',
+      location: 'Pakistan',
+      startDate: 'May 2015',
+      endDate: 'Jul 2018',
+      description: [
+        '- Developed vertical solution for Core CRM and Vessel Service Solutions.',
+        '- Architected Claris hosting for on-premise and cloud-based solutions (AWS).',
+        '- Designed standard coding practices for intenal development team.',
+        '- Managed shared and dedicated hosting for over 300 clients (Installs, estimates, configurations, migrations, and updates).',
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: 'California State University San Marcos',
+      location: 'San Marcos, CA',
+      startDate: '2010',
+      endDate: '2015',
+      description: [
+        "- Bachelor's Degree in Computer Science with a minor in Physics.",
+        '- Graduate coursework in Cyber Security, Data Communications and Networks, Project Management, and Artificial Intelligence.',
+      ],
+    },
+  ],
+
+  skills: [
+    {
+      name: 'Claris FileMaker',
+      description: [
+        '- Certified FileMaker Developer (Versions 14-20)',
+        '- Certified Claris Connect Developer (2023)',
+        '- Third-Party Plugin Integrations (PCI, 360Works, Troi, Goya)',
+        '- API Integrations (REST, cURL, XML, JSON)',
+        '- FileMaker Server, FileMaker Go, FileMaker WebDirect',
+      ],
+    },
+    {
+      name: 'Web Development',
+      description: [
+        '- HTML, CSS, and JavaScript',
+        '- React.JS, Node.JS',
+        '- StackBlitz, Astro, Vite',
+        '- AWS, Lightsail, GoDaddy, CPanel, LetsEncrypt',
+        '- Bootstrap, TailwindCSS, Flex',
+      ],
+    },
+    {
+      name: 'Project Management',
+      description: [
+        '- SSR Documentation',
+        '- Requirement Gathering and Sprint Planning',
+        '- Project planning and time management',
+      ],
+    },
+  ],
+
+  socialMedia: {
+    github: 'https://github.com/JeffHenry',
+    email: 'jeff@jeffreyhenry.me',
+    linkedin: 'https://www.linkedin.com/in/jeffreychenry/',
+  },
+
+  projects: [
+    {
+      title: 'Coming Soon',
+      isFeatured: true,
+      thumbnail: 'public/assets/images/coming-soon.jpg',
+      githubUrl: '',
+      liveUrl: '',
+    },
+  ],
+};
